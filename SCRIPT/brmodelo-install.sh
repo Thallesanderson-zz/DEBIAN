98726633b33926c263c48d652fd9c8ddfd146499
thalles <thallesanderson@hotmail.com>
2020-04-01 18:32:47 -0400

    BRMODELO (DEBIAN BUSTER)
    Instalação do "BRMODELO" versão 3.3.3 no "DEBIAN BUSTER"

---
+++ SCRIPT/brmodelo-install.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+## INSTALAÇÃO DO java (NECESÁRIO PARA RODAR "BR MODELO"). ##
+sudo apt-get install default-jdk -y sudo wget ;
+
+## INSTALAÇÃO DO "BR MODELO". ##
+cd /home/ ; sudo mkdir .app ; cd /home/.app/ && sudo wget -c https://ucd3bf3b3460577ff54024ce5b97.dl.dropboxusercontent.com/cd/0/get/A1AipzdZr4ag98FB6xAaUbSut2tVbyw6K5daFrgkovYwiVvp9W-oReQGFpAr9MnQOGRlHQvuIgYyXeXUV_oW_NCJo0lz_Zi8f491UkYgZlvEi0lUYzGIOOG8tiVXdVnFOIY/file?dl=1# ; sudo cp file?dl=1 BRMODELO.tar.xz ; sudo rm -rf file\?dl\=1 ; sudo tar -Jxf BRMODELO.tar.xz ; sudo rm -rf BRMODELO.tar.xz ; cd BRMODELO/ ; sudo mv BRMODELO.desktop ~/.local/share/applications/ ; cd /home/ sudo chmod 777 .app/* ;
